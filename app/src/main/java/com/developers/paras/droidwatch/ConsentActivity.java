2be609faebaef218d5820a6931314d0428980974
parasthekoder <khandelwalparas8@gmail.com>
2019-07-18 19:55:58 +0530

    Consent added

---
+++ app/src/main/java/com/developers/paras/droidwatch/ConsentActivity.java
@@ -0,0 +1,13 @@
+package com.developers.paras.droidwatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConsentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consent);
+    }
+}
